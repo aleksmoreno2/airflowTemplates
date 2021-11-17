f7b18668f35ae08222babfe50a09898a4c867862
aleksmoreno2 <91718364+aleksmoreno2@users.noreply.github.com>
2021-11-16 22:54:31 -0600

    Add files via upload

---
+++ ins_localuserPurchase_to_postg.py
@@ -0,0 +1,64 @@
+import airflow
+import os
+import psycopg2
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.postgres_hook import PostgresHook
+from datetime import timedelta
+from datetime import datetime
+
+default_args = {
+    'owner': 'alejandra.moreno',
+    'depends_on_past': False,    
+    'start_date': datetime(2021, 10, 29),
+    'retries': 2,
+    'retry_delay': timedelta(minutes=1),
+}
+
+dag = DAG('ins_localuserPurchase_to_postg',
+          default_args=default_args,
+          schedule_interval='@once',
+          catchup=False)
+
+def file_path(relative_path):
+    dir = os.path.dirname(os.path.abspath(__file__))
+    split_path = relative_path.split("/")
+    return split_path
+
+def csvToPostgres():
+    #Open Postgres Connection
+    pg_hook = PostgresHook(postgres_conn_id='postgres_sql')
+    get_postgres_conn = PostgresHook(postgres_conn_id='postgres_sql').get_conn()
+    curr = get_postgres_conn.cursor()
+    # CSV loading to table.
+    with open(file_path("/users/aleks/de-bootcamp-am/user_purchase.csv"), "r")) as f:
+        next(f)
+        curr.copy_from(f, 'user_purchaseZ9', sep=',')
+        get_postgres_conn.commit()
+
+
+#Task create_table
+create_table = PostgresOperator(task_id = 'create_user_purchase_table',
+                         sql ="""
+                            CREATE TABLE IF NOT EXISTS user_purchaseZ9 (
+                            invoice_number VARCHAR(10) PRIMARY KEY,
+                            stock_code VARCHAR(20),
+                            detail VARCHAR(1000),
+                            quantity INT,
+                            invoice_date TIMESTAMP,
+                            unit_price NUMERIC(8,3),
+                            customer_id INT,
+                            country VARCHAR(20));
+                        """,
+                         postgres_conn_id='postgres_sql', 
+                         autocommit=True,
+                         dag= dag)
+
+#Task populate_table
+populate_table = PythonOperator(task_id='csv_to_db',
+                   provide_context=True,
+                   python_callable=csvToPostgres,
+                   dag=dag)
+
+create_table >> populate_table
