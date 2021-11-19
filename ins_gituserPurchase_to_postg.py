7b1c1f1ebfc717a51746ffba4c3fe8c6e7eabcce
aleksmoreno2 <91718364+aleksmoreno2@users.noreply.github.com>
2021-11-18 22:50:24 -0600

    Update ins_gituserPurchase_to_postg.py

--- ins_gituserPurchase_to_postg.py
+++ ins_gituserPurchase_to_postg.py
@@ -42,7 +42,7 @@ def csvToPostgres():
 create_table = PostgresOperator(task_id = 'create_user_purchase_table',
                          sql ="""
                             CREATE TABLE IF NOT EXISTS user_purchases (
-                            invoice_number VARCHAR(10) PRIMARY KEY,
+                            invoice_number VARCHAR(10),
                             stock_code VARCHAR(20),
                             detail VARCHAR(1000),
                             quantity INT,
