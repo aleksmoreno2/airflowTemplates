ff7c043248f195991194d5903d29533082d955c6
aleksmoreno2 <91718364+aleksmoreno2@users.noreply.github.com>
2021-11-19 00:42:17 -0600

    Update ins_localuserPurchase_to_postg.py

--- ins_localuserPurchase_to_postg.py
+++ ins_localuserPurchase_to_postg.py
@@ -22,7 +22,7 @@ dag = DAG('ins_localuserPurchase_to_postg',
           catchup=False)
 
 def file_path(relative_path):
-    dir = os.path.dirname(os.path.abspath(__file__))
+    dir = 'C:\Users\aleks\de-bootcamp-am'
     split_path = relative_path.split("/")
     new_path = os.path.join(dir, *split_path)
     return new_path
@@ -33,7 +33,7 @@ def csvToPostgres():
     get_postgres_conn = PostgresHook(postgres_conn_id='postgres_sql').get_conn()
     curr = get_postgres_conn.cursor()
     # CSV loading to table.
-    with open(os.path("c:/Users/aleks/de-bootcamp-am/user_purchase.csv"), "r") as f:
+    with open(file_path("user_purchase.csv"), "r") as f:
         next(f)
         curr.copy_from(f, 'user_purchases', sep=',')
         get_postgres_conn.commit()
