24f4765aa499abe51f67e252b7588fa51e608be0
aleksmoreno2 <91718364+aleksmoreno2@users.noreply.github.com>
2021-11-21 03:53:04 -0600

    Add files via upload

---
+++ 2postgres_to_gcs.py
@@ -0,0 +1,44 @@
+import os
+from airflow import DAG
+from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
+from datetime import timedelta
+from datetime import datetime
+
+default_args = {
+    ''owner': 'alejandra.moreno',
+    'depends_on_past': False,    
+    'start_date': datetime(2021, 10, 29),
+    'retries': 2,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG('postgres_to_gcs',
+          default_args=default_args,
+          schedule_interval='@once',
+          catchup=False)
+
+GOOGLE_CONN_ID = "google_cloud_project"
+POSTGRES_CONN_ID = "postgres_sql"
+FILENAME = "user_purchase_"
+SQL_QUERY = "select * from user_purchase"
+bucket_name = "de-bootcamp-gcs-raw"
+
+upload_data = PostgresToGCSOperator(
+        task_id="get_data", 
+        sql=SQL_QUERY, 
+        bucket=bucket_name, 
+        filename=FILENAME, 
+        gzip=False, 
+        dag=dag)
+        
+upload_data_server_side_cursor = PostgresToGCSOperator(
+        task_id="get_data_with_server_side_cursor",
+        sql=SQL_QUERY,
+        bucket=bucket_name,
+        filename=FILENAME,
+        gzip=False,
+        use_server_side_cursor=True,
+        export_format='parquet',
+        dag=dag)
+
+upload_data >> upload_data_server_side_cursor
---
+++ 3ins_movieReview_to_raw.py
@@ -0,0 +1,42 @@
+import airflow
+import os
+import psycopg2
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
+from datetime import timedelta
+from datetime import datetime
+
+default_args = {
+    'owner': 'alejandra.moreno',
+    'depends_on_past': False,    
+    'start_date': datetime(2021, 10, 29),
+    'retries': 2,
+    'retry_delay': timedelta(minutes=1),
+}
+
+dag = DAG('ins_movieReview_to_raw',
+          default_args=default_args,
+          schedule_interval='@once',
+          catchup=False)
+
+GOOGLE_CONN_ID = "google_cloud_project"
+BUCKET_SRC = 'de-bootcamp-am_raw_data'
+OBJECT_SRC = 'movie_review.csv'
+BUCKET_DST = "de-bootcamp-gcs-raw"
+OBJECT_DST = '_movie_review.csv'
+
+FILENAME = "user_purchase_"
+
+#Task create_table
+ins_moviereview_to_rawbucket = GCSToGCSOperator(
+    task_id="copy_movieReview_raw_gcs",
+    source_bucket=BUCKET_SRC,
+    source_object=OBJECT_SRC,
+    destination_bucket=BUCKET_DST
+    destination_object=OBJECT_DST
+)
+
+ins_moviereview_to_rawbucket
