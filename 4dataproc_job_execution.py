fb861b38fdc77f54c35bbc85800a7d1deecd1cbf
aleksmoreno2 <91718364+aleksmoreno2@users.noreply.github.com>
2021-11-21 23:13:35 -0600

    Update 4dataproc_job_execution.py

--- 4dataproc_job_execution.py
+++ 4dataproc_job_execution.py
@@ -17,12 +17,12 @@ PROJECT_ID='de-bootcamp-am'
 CLUSTER_CONFIG = {
     "master_config": {
         "num_instances": 1,
-        "machine_type_uri": "n1-standard-4",
+        "machine_type_uri": "n1-standard-2",
         "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 256},
     },
     "worker_config": {
         "num_instances": 2,
-        "machine_type_uri": "n1-standard-4",
+        "machine_type_uri": "n1-standard-2",
         "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 256},
     }
 }
