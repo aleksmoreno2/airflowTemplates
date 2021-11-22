c59fdf296f59d2bc76a4a42d5e3fa3976484bc71
aleksmoreno2 <91718364+aleksmoreno2@users.noreply.github.com>
2021-11-22 01:27:54 -0600

    Update 4dataproc_job_execution.py

--- 4dataproc_job_execution.py
+++ 4dataproc_job_execution.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from datetime import timedelta
+from datetime import datetime
 from airflow.utils.dates import days_ago
 from airflow.providers.google.cloud.operators.dataproc import  DataprocCreateClusterOperator
 from airflow.providers.google.cloud.operators.dataproc import DataprocSubmitJobOperator
