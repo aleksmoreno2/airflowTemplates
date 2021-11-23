0f0759faa02a0ed69fc7bb760c5af50ced773660
aleksmoreno2 <91718364+aleksmoreno2@users.noreply.github.com>
2021-11-23 02:47:16 -0600

    Update 2postgres_to_gcs.py

--- 2postgres_to_gcs.py
+++ 2postgres_to_gcs.py
@@ -19,7 +19,6 @@ dag = DAG('postgres_to_gcs',
           schedule_interval='@once',
           catchup=False)
 
-GOOGLE_CONN_ID = "google_cloud_default"
 POSTGRES_CONN_ID = "postgres_sql"
 FILENAME = "user_purchase_"
 SQL_QUERY = "select * from user_purchase"
