27abb924a8b9aed4e2564eb2b0a57aef4a4a562c
aleksmoreno2 <91718364+aleksmoreno2@users.noreply.github.com>
2021-12-09 03:56:28 -0600

    Update userBehaviorLogic.pyspark.py

--- userBehaviorLogic.pyspark.py
+++ userBehaviorLogic.pyspark.py
@@ -12,5 +12,5 @@ from pyspark.sql.functions import when
 sc = SparkContext()
 spark = SparkSession(sc)
 
-movie_reviewdf = spark.read.csv("gs://de-bootcamp-gcs-staging/user_purchase", header=True, inferSchema=True)
+movie_reviewdf = spark.read.json("gs://de-bootcamp-gcs-staging/user_purchase", header=True, inferSchema=True)
 class_moviewreviewdf = spark.read.option("header", "true").parquet("gs://de-bootcamp-gcs-staging/classificationrevlogic/part-00000-6b24ba42-d635-4611-a7b1-d7b18d0b1cfe-c000.snappy.parquet")
