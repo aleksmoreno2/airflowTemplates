ef70af6cd5144dbc2e708920a7acee500b7b4b2b
aleksmoreno2 <91718364+aleksmoreno2@users.noreply.github.com>
2021-12-09 01:15:01 -0600

    Update 1ins_userPurchase_to_postg.py

--- 1ins_userPurchase_to_postg.py
+++ 1ins_userPurchase_to_postg.py
@@ -14,8 +14,8 @@ default_args = {
     'owner': 'alejandra.moreno',
     'depends_on_past': False,    
     'start_date': datetime(2021, 10, 29),
-    'retries': 2,
-    'retry_delay': timedelta(minutes=1),
+    'retries': 1,
+    'retry_delay': timedelta(seconds=10),
 }
 
 dag = DAG('ins_userPurchase_to_postg',
@@ -27,38 +27,6 @@ GOOGLE_CONN_ID = "google_cloud_default"
 POSTGRES_CONN_ID = "postgres_sql"
 bucket_name = "de-bootcamp-am_raw_data"
 bucket_file = 'user_purchase.csv'
-
-#def read_file(filename):
-#     google_cloud_storage_conn_id='google_cloud_default'
-#     gcs_file = gcs.open(filename)
-#     contents = gcs_file.read()
-#     gcs_file.close()
-#     self.response.write(contents)
- 
-#def __init__(self,
-#                 google_cloud_storage_conn_id='google_cloud_default',
-#                 delegate_to=None):
-#        super(GoogleCloudStorageHook, self).__init__(google_cloud_storage_conn_id,
-#                                                     delegate_to)
-
-#def get_conn(self):
-#        """
-#        Returns a Google Cloud Storage service object.
-#        """
-#        http_authorized = self._authorize()
-#        return build('storage', 'v1', http=http_authorized)    
-    
-#def download(bucket, object, filename=None):
-#        g_hook = GoogleCloudStorageHook(gcp_conn_id=GOOGLE_CONN_ID,)
-#        bucket = g_hook.bucket(bucket)
-#        blob = bucket.blob(blob_name=object)
-
-#        if filename:
-#            blob.download_to_filename(filename)
-#            self.log.info('File downloaded to %s', filename)
-#            return filename
-#        else:
-#            return blob.download_as_string()
 
 def csvToPostgres():
     #Open Postgres Connection
